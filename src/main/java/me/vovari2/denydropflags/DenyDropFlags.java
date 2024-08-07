7bbd45f0fd4a65b4e46799093b6fe94e7318158e
Vovari2 <vovari4ek2@gmail.com>
2024-08-07 17:09:49 +0300

    initial commit

---
+++ src/main/java/me/vovari2/denydropflags/DenyDropFlags.java
@@ -0,0 +1,17 @@
+package me.vovari2.denydropflags;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DenyDropFlags extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
